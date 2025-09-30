b0c2fe827fff87ed666c170af0e826d5823418ad
fa <aider2000@mail.ru>
2025-09-30 20:19:37 +0300

    setup react-router-dom, configure Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Counter } from "./components/Counter"
+import { Routes, Route, Link } from "react-router-dom"
 import "./index.scss"
+import MainPage from "./pages/MainPage/MainPage"
+import AboutPage from "./pages/AboutPage/AboutPage"
 
 const App = () => {
   return (
     <div className="app">
-      <div>afasfw3www</div>
-      <Counter/>
+      <Link to={"/"}>About</Link>
+      <Link to={"/mainpage"}>MainPage</Link>
+      <Routes>
+        <Route path="/" element={<AboutPage/>}/>
+        <Route path="/mainpage" element={<MainPage/>}/>
+      </Routes>
     </div>
   )
 }
