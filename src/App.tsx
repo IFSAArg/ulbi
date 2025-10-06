91bf56eecaa87bf5447ceaf824de91017c417bf7
fa <aider2000@mail.ru>
2025-10-06 18:27:59 +0300

    organize styles, add themes, setup theme-context and theme-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Suspense } from "react"
+import { Suspense, useContext, useState } from "react"
 import { Routes, Route, Link } from "react-router-dom"
-import "./index.scss"
+import "./styles/index.scss"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
+import { Theme, ThemeContext } from "./theme/ThemeContext"
 
 const App = () => {
+  const {theme, setTheme} = useContext(ThemeContext)
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? Theme.LIGHT : Theme.DARK)
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>About</Link>
       <Link to={"/mainpage"}>MainPage</Link>
       <Suspense fallback={<div>Loading...</div>}>
--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -2,6 +2,5 @@ import { lazy } from "react";
 
 export const AboutPageAsync = lazy(() => new Promise(resolve => {
   // @ts-ignore
-  // Это сделано исключительно дя имитации загрузки
   setTimeout(() => resolve(import('./AboutPage')), 1500)
 }))
